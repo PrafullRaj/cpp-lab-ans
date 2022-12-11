bf5b88c9fb09d356ede04059697f53806c46ab43
prafull raj <99156529+PrafullRaj@users.noreply.github.com>
2022-12-11 13:08:07 +0530

    Create Write a program in C++ to highlight the difference between overloaded assignment operator  and copy construct.cpp

---
+++ WEEK 9/Write a program in C++ to highlight the difference between overloaded assignment operator  and copy construct.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class circle
+{
+private:
+    int radius;
+    float x, y;
+
+public:
+    circle(){}
+    circle(int rr, float xx, float yy)
+    {
+        radius = rr;
+        x = xx;
+        y = yy;
+    }
+    circle operator=(circle &c)
+    {
+        cout << endl
+             << "Assignment operator invoked";
+        radius = c.radius;
+        x = c.x;
+        y = c.y;
+        
+    }
+    circle(circle &c)
+    {
+        cout << endl
+             << "copy constructor invoked";
+        radius = c.radius;
+        x = c.x;
+        y = c.y;
+    }
+    void showdata()
+    {
+        cout << endl
+             << "Radius = " << radius;
+        cout << endl
+             << "X-Coordinate=" << x;
+        cout << endl
+             << "Y-Coordinate=" << y;
+    }
+};
+int main()
+{
+    circle c1(10, 2.5, 2.5);
+    circle c2, c4;
+    c4 = c1;
+    c2 = c1;
+    circle c3 = c1;
+    c1.showdata(); cout<<endl;
+    c2.showdata(); cout<<endl;
+    c3.showdata(); cout<<endl;
+    c4.showdata(); cout<<endl;
+    return 0;
+}
